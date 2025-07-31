f8bf3fbc836433fc3f525b60ba33e07ef80372c1
LuisAceredaDebian <lacereda@formacion.cic.es>
2025-07-31 11:38:58 +0200

    advice cambiado

--- src/main/java/es/cic/curso25/proy009/controller/Advice.java
+++ src/main/java/es/cic/curso25/proy009/controller/Advice.java
@@ -2,18 +2,18 @@ package es.cic.curso25.proy009.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
  
-@ControllerAdvice
+@RestControllerAdvice
 public class Advice {
  
     @ExceptionHandler(CreationException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleCreacionException(CreationException e) {
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
+                .status(HttpStatus.BAD_REQUEST)
                 .body("Error: " + e.getMessage());
     } 
 }
